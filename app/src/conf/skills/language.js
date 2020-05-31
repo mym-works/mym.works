8fed0882340d6ee3fa3966b4b48bb020f3af3420
KoheiMayama <mym.kohei@gmail.com>
2020-05-31 15:52:56 +0900

    プログラミング言語のconfを追加

---
+++ app/src/conf/skills/language.js
@@ -0,0 +1,23 @@
+import aws_logo from '../../img/skills/cloud/aws.png';
+import gcp_logo from '../../img/skills/cloud/gcp.png';
+
+const languageData = [
+  {
+    img: aws_logo,
+    title: 'Python',
+  },
+  {
+    img: gcp_logo,
+    title: 'Go',
+  },
+  {
+    img: gcp_logo,
+    title: 'Ruby',
+  },
+  {
+    img: gcp_logo,
+    title: 'Node.js',
+  },
+];
+
+export default languageData;
