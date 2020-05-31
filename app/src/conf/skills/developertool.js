fecc75b6216cb40ef7c08b2882b714bbe75b4fe0
KoheiMayama <mym.kohei@gmail.com>
2020-05-31 16:39:06 +0900

    developertoolのconfを変更

--- app/src/conf/skills/developertool.js
+++ app/src/conf/skills/developertool.js
@@ -1,33 +1,38 @@
-import aws_logo from '../../img/skills/cloud/aws.png';
-import gcp_logo from '../../img/skills/cloud/gcp.png';
+import ghe_logo from '../../img/skills/developertool/github-enterprise.png';
+import datadog_logo from '../../img/skills/developertool/datadog.png';
+import mackerel_logo from '../../img/skills/developertool/mackerel.png';
+import circleci_logo from '../../img/skills/developertool/circleci.png';
+import travisci_logo from '../../img/skills/developertool/travisci.png';
+import twilio_logo from '../../img/skills/developertool/twilio.png';
+import vuls_logo from '../../img/skills/developertool/vuls.png';
 
 const developerToolData = [
   {
-    img: gcp_logo,
+    img: ghe_logo,
     title: 'GitHub Enterprise',
   },
   {
-    img: aws_logo,
+    img: datadog_logo,
     title: 'Datadog',
   },
   {
-    img: aws_logo,
-    title: 'Mackerel',
+    img: mackerel_logo,
+    title: 'mackerel',
   },
   {
-    img: gcp_logo,
+    img: circleci_logo,
     title: 'CircleCI',
   },
   {
-    img: gcp_logo,
+    img: travisci_logo,
     title: 'TravisCI',
   },
   {
-    img: gcp_logo,
+    img: twilio_logo,
     title: 'Twilio',
   },
   {
-    img: gcp_logo,
+    img: vuls_logo,
     title: 'Vuls',
   },
 ];
